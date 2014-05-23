0f500ced07170da3ee6d318539e5f8ed2c854c22
Horia <Horia@wifi-etu-0243.univ-lyon1.fr>
2014-05-23 16:06:49 +0200

    Vue en trois colonnes

--- src/vue/Vue.java
+++ src/vue/Vue.java
@@ -7,7 +7,9 @@ package vue;
 import Observe.ObserverGrille;
 import Observe.ObserverTemps;
 import controleur.TetrisControler;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,14 +20,14 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.BorderFactory;
-
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JMenu;
-
 import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicComboBoxUI.ItemHandler;
 import modele.Grille;
@@ -37,12 +39,17 @@ import modele.Grille;
 public class Vue extends JFrame implements ObserverGrille, ObserverTemps{
 
     private TetrisControler controler;
+    private JPanel container = new JPanel();
     GridLayout layout = new GridLayout(20, 10);
     JPanel grille_pan;
+    JPanel gauche;
+    JPanel droite;
 
     public Vue(TetrisControler controleur) {
         super();
         this.controler = controleur;
+    
+        
         build();
         addWindowListener(new WindowAdapter() {
             @Override
@@ -61,61 +68,92 @@ public class Vue extends JFrame implements ObserverGrille, ObserverTemps{
 
         JMenu m = new JMenu("Jeu");
 
-        JMenuItem mi = new JMenuItem("Partie");
+        JMenuItem mi = new JMenuItem("Nouvelle Partie");
+        
+        JMenuItem min = new JMenuItem("Pause");
 
         //ItemListener i = new Item
         m.add(mi);
+        m.add(min);
         jm.add(m);
         setJMenuBar(jm);
 
-        setTitle("Ma première fenêtre");
-        setSize(300, 600);
+        setTitle("Tetris");
+        setSize(816, 700);
         setResizable(false);
+        
+        //--Grille Centrale
+        grille_pan = new JPanel();
+        grille_pan.setPreferredSize(new Dimension(350,700)); 
+        grille_pan.setLayout(layout);
 
-        grille_pan = afficherGrille(null);
+        Border blackline = BorderFactory.createLineBorder(Color.darkGray, 1);
+
+        for (int i = 0; i < 200; i++) {
+            JComponent ptest = new Case(Color.lightGray);
+            ptest.setBorder(blackline);
+            grille_pan.add(ptest);
+        }
+        
+        
+        afficherGrille(null);
+        afficherGauche();
+        afficherDroite();
         addKeyListener(new Fleche());
-        setContentPane(grille_pan);
+        setContentPane(container);
+        
+        container.setLayout(new BorderLayout());
+        
+        container.add(grille_pan, BorderLayout.CENTER);
+        container.add(gauche, BorderLayout.WEST);
+        container.add(droite, BorderLayout.EAST);
         setVisible(true);
+        
         
     }
 
-    public JPanel afficherGrille(Grille grille) {
-        JPanel pan = new JPanel();
-        GridLayout layout = new GridLayout(20, 10);
-        pan.setLayout(layout);
-
-        Border blackline = BorderFactory.createLineBorder(Color.black, 1);
-
-        for (int i = 0; i < 200; i++) {
-            JComponent ptest = new Case(Color.white);
-            ptest.setBorder(blackline);
-            pan.add(ptest);
-        }
+    public void afficherGrille(Grille grille) {
+        
         if (grille != null) {
             for (int y = 0; y < grille.getLongueur(); y++) {
                 for (int x = 0; x < grille.getLargeur(); x++) {
                     if(grille.getGrille()[y][x]==null){
-                        pan.getComponent(x + y * 10).setBackground(Color.WHITE);
+                        grille_pan.getComponent(x + y * 10).setBackground(Color.lightGray);
                     }
                     else{
-                        pan.getComponent(x + y * 10).setBackground(grille.getGrille()[y][x].getCouleur());
+                        grille_pan.getComponent(x + y * 10).setBackground(grille.getGrille()[y][x].getCouleur());
                     }
                     
                 }
             }
              
         }
-       // pan.getComponent((x-1) * 10 + (y-1)).setBackground(Color.red);
-        //pan.getComponentAt(10, 10).setBackground(Color.red);
-
-        pan.setBorder(blackline);
-        grille_pan=pan;
-        setContentPane(grille_pan);
-        grille_pan.repaint();
-        validate();
-        //setContentPane(grille_pan);
-        return pan;
     }
+    
+    public void afficherDroite(){
+        
+        droite = new JPanel();
+        Border blackl = BorderFactory.createLineBorder(Color.darkGray, 2);
+        
+        droite.setPreferredSize(new Dimension(233,700)); 
+        droite.setLayout(layout);
+        
+        droite.setBorder(blackl);
+        
+        JLabel next = new JLabel("next");
+        next.setSize(100,100);
+        next.setBorder(blackl);
+        droite.add(next, BorderLayout.CENTER);
+    }
+    
+    public void afficherGauche(){
+        gauche = new JPanel();
+        gauche.setPreferredSize(new Dimension(233,700)); 
+        gauche.setLayout(layout);
+        Border blackl = BorderFactory.createLineBorder(Color.darkGray, 2);
+        gauche.setBorder(blackl);
+    }
+    
     
      public void update(Grille grille)
      {
@@ -126,41 +164,47 @@ public class Vue extends JFrame implements ObserverGrille, ObserverTemps{
      {
       controler.control("B");   
      }
-class Fleche implements KeyListener{ 
-    public void keyReleased(KeyEvent e) {
-                
-  }
-    public void keyPressed(KeyEvent e) {
-     if(e.getExtendedKeyCode()==KeyEvent.VK_DOWN)
-     { controler.control("B");}
-     if(e.getExtendedKeyCode()==KeyEvent.VK_LEFT)
-     {
-         controler.control("G");
-     }
-      if(e.getExtendedKeyCode()==KeyEvent.VK_RIGHT)
-     {
-         controler.control("D");
-     }
-     if(e.getExtendedKeyCode()==KeyEvent.VK_UP)
-     {
-         controler.control("H");
-     }
-     if(e.getExtendedKeyCode()==KeyEvent.VK_P)
-     {
-         controler.control("Pause");
-     }
-      if(e.getExtendedKeyCode()==KeyEvent.VK_J)
-     {
-         controler.control("Jouer");
-     }
-      
-        
-                    
-  }
-    public void keyTyped(KeyEvent e) {
-      //On affiche le chiffre en plus dans le label
+
+     
+     
+     
+    class Fleche implements KeyListener{ 
+
+        public void keyReleased(KeyEvent e) {
+
+      }
+        public void keyPressed(KeyEvent e) {
+         if(e.getExtendedKeyCode()==KeyEvent.VK_DOWN)
+         { controler.control("B");}
+         if(e.getExtendedKeyCode()==KeyEvent.VK_LEFT)
+         {
+             controler.control("G");
+         }
+          if(e.getExtendedKeyCode()==KeyEvent.VK_RIGHT)
+         {
+             controler.control("D");
+         }
+         if(e.getExtendedKeyCode()==KeyEvent.VK_UP)
+         {
+             controler.control("H");
+         }
+         if(e.getExtendedKeyCode()==KeyEvent.VK_P)
+         {
+             controler.control("Pause");
+         }
+          if(e.getExtendedKeyCode()==KeyEvent.VK_J)
+         {
+             controler.control("Jouer");
+         }
+
+
+
+      }
+    
+        public void keyTyped(KeyEvent e) {
+        //On affiche le chiffre en plus dans le label
        
-  }
-}
+        }
+    }
 }
 
